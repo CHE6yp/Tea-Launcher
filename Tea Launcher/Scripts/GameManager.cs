fb94e6645aa11aeb4debd52b22de1c0427111b42
che6yp <che6yp@gmail.com>
2018-02-26 21:03:09 +0300

    Getting file paths

---
+++ Tea Launcher/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Windows;
+using Newtonsoft.Json;
+
+
+namespace Tea_Launcher
+{
+    class GameManager
+    {
+
+        public static void GetFileNames()
+        {
+            // Create a request for the URL. 		
+            WebRequest request = WebRequest.Create("http://170295.simplecloud.ru/?n=Burger%20Joint");
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Display the status.
+            string SD = response.StatusDescription;
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            
+            // Cleanup the streams and the response.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+
+            //var paths = JsonConvert.DeserializeObject(responseFromServer);
+            string[] paths = responseFromServer.Split('!');
+
+            Window mainWindow = new Window();
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    mainWindow = window;
+                }
+            }
+            int i = 0;
+            foreach (string path in paths)
+            {
+                (mainWindow as MainWindow).textBox.AppendText(i++.ToString()+path+"\n");
+            }
+        }
+    }
+}
